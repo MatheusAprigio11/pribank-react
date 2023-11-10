c75b5b23663a641163d1bd1699bf583a8eac3ebf
Matheus Aprigio <matheusaprigio11>
2023-11-09 22:33:38 -0300

    updt responsive

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -11,28 +11,29 @@ const Navbar = () => {
 
   return (
     <div className='z-50 w-full fixed top-0 left-0'>
-        <div className='md:flex flex items-center justify-center gap-96 bg-transparent py-4 md:px-10 px-7'>
+        <div className='md:flex flex items-center justify-center md:gap-10 lg:gap-16 xl:gap-36 2xl:gap-80
+         bg-transparent py-4 md:px-10 px-7'>
         <div className='cursor-pointer flex items-center'>
-        <img src='src/assets/images/Group 1.png' className='w-13 h-auto'/>
+        <img src='src/assets/images/Group 1.png' className='w-13 md:w-12 h-auto'/>
         </div>
         <div onClick={()=>
             setOpen(!open)
             } className='text-3x1 absolute right-8 top-6 cursor-pointer md:hidden'>
             <ion-icon name={open ? 'close':'menu'}></ion-icon>
         </div>
-        <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-transparent md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-16 opacity-100':'top-[-480px]'} md:opacity-100 opacity-0`}>
+        <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-transparent md:z-auto z-[-1] left-0 w-auto md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-16 opacity-100':'left-[-480px]'} md:opacity-100 opacity-0`}>
             {    
                 Links.map((link)=>(
-                    <li key={link.name} className='md:ml-8 text-xl md:my-0 my-7'>
-                        <a href={link.link} className='text-gray-800 hover:text-gray-400 duration-500'>{link.name}</a>
+                    <li key={link.name} className='md:ml-8 text-base md:my-0 my-7'>
+                        <a href={link.link} className='text-white font-lexend font-semibold text-xs p-7 md:text-less hover:text-gray-400 duration-500'>{link.name}</a>
                     </li>
                 ))
             }
             
         </ul>
         <Button
-                title='Logar'
-                classname='bg-indigo-600 text-white font-poppins py-2 px-6 rounded md:ml-8 hover:bg-indigo-400 duration-500'
+                title='Faça parte'
+                classname='bg-[#ff364e] w-40 text-white md:text-less md:w-28 2xl:text-base font-poppins py-2 px-6 rounded-2xl hover:bg-[#752932] duration-500 '
             />
         </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,17 @@ export default {
     extend: {},
     fontFamily:{
       poppins : ['Poppins', 'sans-serif'],
+      lexend : ['Lexend', 'sans-serif'],
+    },
+    fontSize:{
+      less : '0.6rem',
+      sm: '0.8rem',
+      base: '1rem',
+      xl: '1.25rem',
+      '2xl': '1.563rem',
+      '3xl': '1.953rem',
+      '4xl': '2.441rem',
+      '5xl': '3.052rem',
     },
   plugins: [],
   }
