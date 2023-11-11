822c1780c850a49498ce996de65fc376d1c2beae
Matheus Aprigio <matheusaprigio11>
2023-11-10 23:22:58 -0300

    updt

---
+++ src/Components/Cards/CardTwo.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const CardTwo = () => {
+  return (
+    <div className='bg-[#949494] bg-opacity-50 flex pb-24'>
+        <div className='pl-24 pt-24 float-right'>
+            <img src="src/assets/images/card2.png" alt="Card" className='w-[750px] h-[500px]'/>
+        </div>
+        <div className='text-4xl'>
+        </div>
+    </div>
+  )
+}
+
+export default CardTwo
--- src/Screens/HomeScreen/HomeScreen.jsx
+++ src/Screens/HomeScreen/HomeScreen.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Card from '../../Components/Cards/Card'
+import CardTwo from '../../Components/Cards/CardTwo'
 
 const HomeScreen = () => {
   return (
     
     <div>
       <Card />
+      <CardTwo />
     </div>
   )
 }
